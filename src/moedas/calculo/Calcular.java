9ff7950833303364bca21efac0fdc0ada27b6807
Victor Barros <89825674+Gnuk935@users.noreply.github.com>
2022-10-26 00:28:08 -0300

    a

--- src/moedas/calculo/Calcular.java
+++ src/moedas/calculo/Calcular.java
@@ -2,13 +2,12 @@ package moedas.calculo;
 
 import moedas.qual.Valores;
 
+
 public class Calcular {
 	// Clase para cacular as moedas
 
 	Valores v = new Valores();
 	
-	public static inicio() {
-		
-	}
+	
 	
 }
