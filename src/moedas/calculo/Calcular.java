fdb400f3309ee02f557911bc05c5395ce4232d62
Victor Barros <89825674+Gnuk935@users.noreply.github.com>
2022-10-28 12:34:55 -0300

    Funcionando

--- src/moedas/calculo/Calcular.java
+++ src/moedas/calculo/Calcular.java
@@ -10,51 +10,138 @@ public class Calcular {
 
 // Clase para cacular as moedas
 	public static void main(String[] args) {
-		Valores v = new Valores(); // Define os valores
 		Tela view = new Tela(); // A tela
-		
-		
+		/*
+		 * Definindo os valores de cada moeda
+		 */
+
+		Valores v = new Valores();
+		Valores cinco = new Valores();
+		Valores dez = new Valores();
+		Valores vinte = new Valores();
+		Valores cinque = new Valores();
+		Valores um = new Valores();
+
 		try (Scanner s = new Scanner(System.in)) {
 			view.inicio(); // Link start
+			view.espaco();
 
 			/*
 			 * Começa a brincadeira
 			 */
 
-			view.defineN(); // numero de colunas
-			v.setN(s.nextInt());
+			// Definindo 5
+			view.defineCincoR(); // numero de colunas
+			cinco.setN(s.nextInt());
+			view.branco();
 
-			view.defineR(); // numero de linhas
-			v.setR(s.nextInt());
-
+			view.defineCincoN(); // numero de linhas
+			cinco.setR(s.nextInt());
+			view.branco();
 			// Confirmação sobre A
 			view.sobreA();
 			x = s.nextInt();
 			if (x == 1) {
-				view.defineA();
-				v.setA(s.nextInt());
+				view.defineCincoA();
+				cinco.setA(s.nextInt());
+			} else if (x == 2) {
+
+			}
+
+			view.espaco();
+
+			// Defindo dez
+			view.defineDezR(); // numero de colunas
+			dez.setN(s.nextInt());
+			view.branco();
+
+			view.defineDezN(); // numero de linhas
+			dez.setR(s.nextInt());
+			view.branco();
+			// Confirmação sobre A
+			view.sobreA();
+			x = s.nextInt();
+			if (x == 1) {
+				view.defineDezA();
+				dez.setA(s.nextInt());
+			} else if (x == 2) {
+
+			}
+
+			view.espaco();
+
+			// Defindo vinte e cinco
+			view.defineVinteR(); // numero de colunas
+			vinte.setN(s.nextInt());
+			view.branco();
+			view.defineVinteN(); // numero de linhas
+			vinte.setR(s.nextInt());
+			view.branco();
+			// Confirmação sobre A
+			view.sobreA();
+			x = s.nextInt();
+			if (x == 1) {
+				view.defineVinteA();
+				vinte.setA(s.nextInt());
+			} else if (x == 2) {
+
+			}
+
+			view.espaco();
+
+			// Cinquenta centavos
+			view.defineCinquentaR(); // numero de colunas
+			cinque.setN(s.nextInt());
+			view.branco();
+			view.defineCinquentaN(); // numero de linhas
+			cinque.setR(s.nextInt());
+			view.branco();
+			// Confirmação sobre A
+			view.sobreA();
+			x = s.nextInt();
+			if (x == 1) {
+				view.defineCinquentaA();
+				cinque.setA(s.nextInt());
+			} else if (x == 2) {
+
+			}
+
+			view.espaco();
+
+			// Define um real
+			view.defineUmR(); // numero de colunas
+			um.setN(s.nextInt());
+			view.branco();
+			view.defineUmN(); // numero de linhas
+			um.setR(s.nextInt());
+			view.branco();
+			// Confirmação sobre A
+			view.sobreA();
+			x = s.nextInt();
+			if (x == 1) {
+				view.defineUmA();
+				um.setA(s.nextInt());
+			} else if (x == 2) {
+
 			}
 		}
 
 		view.espaco(); // Apenas um espaço kk
-		v.setnR(v.getN() * v.getR() - v.getA()); // Seta o valor de numero real
-		
-		v.setnRc(v.getnR() * 0.5); //Cinco centavos
-		v.setnRd(v.getnR() * 0.10); //Dez centavos
-		v.setnRv(v.getnR() * 0.25); // Vinte e cinco centavos
-		v.setnRcc(v.getnR() * 0.50); //Cinquenta centavos
-		v.setnRu(v.getnR() * 1); // Um real
-		
-		//Calculo final
-		
+
+		v.setnRc((cinco.getR() * cinco.getN() - cinco.getA()) * 0.5); // Cinco centavos
+		v.setnRd((dez.getR() * dez.getN() - dez.getA()) * 0.10); // Dez centavos
+		v.setnRv((vinte.getR() * vinte.getN() - vinte.getA()) * 0.25); // Vinte e cinco centavos
+		v.setnRcc((cinque.getR() * cinque.getN() - cinque.getA()) * 0.50); // Cinquenta centavos
+		v.setnRu((um.getR() * um.getN() - um.getA()) * 1); // Um real
+
+		// Calculo final
+
 		v.setresultado(v.getnRc() + v.getnRd() + v.getnRv() + v.getnRcc() + v.getnRu());
-		
+
 		view.esp();
-		view.fim();
-		
+		System.out.println("O resultado é: R$" + v.getresultado());
 
 	}
-	
 
 	public static int getX() {
 		return x;
--- src/moedas/view/Tela.java
+++ src/moedas/view/Tela.java
@@ -1,9 +1,6 @@
 package moedas.view;
 
-import moedas.qual.Valores;
-
 public class Tela {
-	Valores v = new Valores();
 	public void inicio() {
 		System.out.println("========================================================================");
 		System.out.println("Olá! Este é um sistema basico de contagem de moedas");
@@ -11,41 +8,98 @@ public class Tela {
 		System.out.println("n = linha");
 		System.out.println("r = coluna");
 		System.out.println("a = este valor é para o caso de não ter uma coluna ou linha incompleta");
+		System.out.println("Cada esses valores são setados a cada valor de moeda!");
 		System.out.println("========================================================================");
 		System.out.println("Por exemplo: 63 moedas de 10 centavos");
-		System.out.println("Então eu as organizo em 8 colunas por 8 linhas, menos 1 linha pois falta uma moeda para fechar a coluna");
+		System.out.println(
+				"Então eu as organizo em 8 colunas por 8 linhas, menos 1 linha pois falta uma moeda para fechar a coluna");
 		System.out.println("Ou seja n = 8 r = 8 a = 1");
 	}
-	
-	public void defineN() {
-		System.out.println("Informe o valor de n (numero de colunas)");
+
+	public void defineCincoN() {
+
+		System.out.println("Informe o valor de n para Cinco centavos (numero de colunas)");
 	}
-	
-	public void defineR() {
-		System.out.println("Informe o valor de r (numero de linhas)");
+
+	public void defineCincoR() {
+		System.out.println("===CINCO CENTAVOS===");
+		System.out.println("Informe o valor de r para Cinco centavos (numero de linhas)");
 	}
-	
-	public void defineA() {
-		System.out.println("Informe o valor de a (deixe 0 caso não tenha");
+
+	public void defineCincoA() {
+		System.out.println("Informe o valor de a para Cinco centavos (deixe 0 caso não tenha");
 	}
-	
+
+	public void defineDezN() {
+		System.out.println("Informe o valor de n para Dez centavos (numero de colunas)");
+	}
+
+	public void defineDezR() {
+		System.out.println("===DEZ CENTAVOS===");
+		System.out.println("Informe o valor de r para Dez centavos (numero de linhas)");
+	}
+
+	public void defineDezA() {
+		System.out.println("Informe o valor de a para Dez centavos (deixe 0 caso não tenha");
+	}
+
+	public void defineVinteN() {
+		System.out.println("Informe o valor de n para Vinte e cinco centavos (numero de colunas)");
+	}
+
+	public void defineVinteR() {
+		System.out.println("===VINTE E CINCO CENTAVOS===");
+		System.out.println("Informe o valor de r para Vinte e cinco centavos (numero de linhas)");
+	}
+
+	public void defineVinteA() {
+		System.out.println("Informe o valor de a para Vinte e cinco centavos (deixe 0 caso não tenha");
+	}
+
+	public void defineCinquentaN() {
+		System.out.println("Informe o valor de n para Cinquenta centavos (numero de colunas)");
+	}
+
+	public void defineCinquentaR() {
+		System.out.println("===CINQUENTA CENTAVOS===");
+		System.out.println("Informe o valor de r para Cinquenta centavos (numero de linhas)");
+	}
+
+	public void defineCinquentaA() {
+		System.out.println("Informe o valor de a para Cinquenta centavos (deixe 0 caso não tenha");
+	}
+
+	public void defineUmN() {
+		System.out.println("Informe o valor de n para Um real (numero de colunas)");
+	}
+
+	public void defineUmR() {
+		System.out.println("===UM REAL===");
+		System.out.println("Informe o valor de r para Um real (numero de linhas)");
+	}
+
+	public void defineUmA() {
+		System.out.println("Informe o valor de a para Um real (deixe 0 caso não tenha");
+	}
+
 	public void sobreA() {
 		System.out.println("Você deseja atribuir um valor para A?");
-		System.out.println("Digite 1 para confirmar");
+		System.out.println("Digite 1 para SIM");
+		System.out.println("Digite 2 para NÃO");
 	}
-	
-	public void espaco(){
+
+	public void espaco() {
 		System.out.println(" ");
 		System.out.println("========================================================================");
 		System.out.println(" ");
 	}
-	
+
+	public void branco() {
+		System.out.println(" ");
+	}
+
 	public void esp() {
 		System.out.println("Estamos calculando, aguarde...");
 	}
-	
-	//TODO fazer a aba de resultado quando necessario!
-	public void fim() {
-		System.out.println("Seu resultado é" + v.getresultado());
-	}
+
 }
